e4cd1df243ebd8f4bd84395657cad0e283206e0c
차유나 <ckdbsk12@gmail.com>
2022-09-23 00:22:06 +0900

    #2 vue, spring 8081포트로 연동

--- vue-realworld/src/common/config.js
+++ vue-realworld/src/common/config.js
@@ -1,2 +1,3 @@
-export const API_URL = "https://conduit.productionready.io/api";
+//export const API_URL = "https://conduit.productionready.io/api";
+export const API_URL = "http://localhost:8081";
 export default API_URL;
